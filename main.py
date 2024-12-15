a6858f85f3b1325bc241c02a255571a3323d6c36
tatapudisairam <156507925+tatapudisairam@users.noreply.github.com>
2024-12-15 20:34:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,5 @@ def predict():
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({'error': str(e)}), 400
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
